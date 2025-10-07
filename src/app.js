3e8acfb4a7c48bc694db761559659247736e625d
Arjun1536 <arjun15singh36@gmail.com>
2025-10-07 14:49:23 +0530

    mongoose model get, post,put,patch . All explore one by one

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const express = require("express")
 const connectDB  = require('./config/database')
 const User = require("./models/user")
 const app = express()
+app.use(express.json())  //! adding middleware to read/update/push data in JSON
 
 const Port = 3000;
 
 connectDB().then(()=>{
     console.log("Database connect successfully")
-    app.listen(Port, ()=>{ //litle beter way to connect db then server
+    app.listen(Port, ()=>{ //^litle beter way to connect db then server
     console.log(`the server runinng on ${Port}`)
 })
 })
@@ -16,23 +17,81 @@ connectDB().then(()=>{
     console.log(err)
 })
 
-app.post("/user",(req,res)=>{
-   const user = new User({
-     firstname:"Arjun",
-    lastname:"Singh",
-    role:"reactJS developer",
-    age:"25"
-   })
+app.post("/signup",async(req,res)=>{
+const user = new User(req.body)
 
-   try{
-user.save()
-res.send("data successfully added")
-   }
-   catch(err){
-    res.status(400).send("some error",err)
-   }
+try{
+    await user.save()
+    res.send("data saved successfully")
+}
+catch(err){
+res.status(400).send("Data not added successfully")
+}
 
 })
+
+//* find  role using model.find() and use model.findOne()
+
+app.get("/getuser",async(req,res)=>{
+    const newUserRole = req.body.role;
+
+    try{
+        const user = await User.find({role:newUserRole})
+        res.send(user)
+    }
+    catch(err){
+        res.status(404).send("something error in code",err)
+    }
+})
+
+//* delete the user using findDeleteOne()
+
+app.delete("/delete_user", async(req,res)=>{
+    const userId = req.body.userId
+    try{
+        const user = await User.findByIdAndDelete(userId)
+        res.status(200).send("user deleted successfully")
+    }
+    catch(err){
+        res.status(400).send("something error with ",err)
+    }
+})
+
+//*  update user data using FindbyIdAndUpdate()
+
+app.patch("/update_user",async(req,res)=>{
+   const {id,...data} = req.body
+   try{
+    const updated_data = await User.findByIdAndUpdate(id,data,{new:true})  //^new: false (default): returns the old document (before updating)
+    res.status(200).send({message: updated_data})                          //^ new: true: returns the new updated document (after updating)
+   }
+   catch(err){
+    res.status(404).send({message: "some error in code",err})
+   }
+})
+
+
+
+
+
+
+// app.post("/user",(req,res)=>{
+//    const user = new User({
+//      firstname:"Arjun",
+//     lastname:"Singh",
+//     role:"reactJS developer",
+//     age:"25"
+//    })
+
+//    try{
+// user.save()
+// res.send("data successfully added")
+//    }
+//    catch(err){
+//     res.status(400).send("some error",err)
+//    }
+
+// })
 
 
 // app.use('/',(req,res)=>{
